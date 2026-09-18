9dd0be812b5c9f8d58687756bc9ba9496055e300
C29999 <1050366415@qq.com>
2026-09-18 14:15:45 +0800

    舵机打死阈值从6改为8

--- user/isr.c
+++ user/isr.c
@@ -64,12 +64,12 @@ IFX_INTERRUPT(cc61_pit_ch0_isr, 0, CCU6_1_CH0_ISR_PRIORITY)
     pit_clear_flag(CCU61_CH0);
     angle = quadradic_pid_solve(&servo_pid, pure_angle);
     // 误差绝对值 > 6 时舵机直接打死（立即打满 ±SMOTOR_LIMIT，不走低通）
-    if (image_error_filter > 6)
+    if (image_error_filter > 8)
     {
         servo_set( SMOTOR_LIMIT);
         return;
     }
-    if (image_error_filter < -6)
+    if (image_error_filter < -8)
     {
         servo_set(-SMOTOR_LIMIT);
         return;
